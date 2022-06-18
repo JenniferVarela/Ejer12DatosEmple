d0c796d284c5c1f48068c81229de08113eed0bed
JenniferVarela <jnnfrvarela@gmail.com>
2022-06-17 22:52:03 -0600

    Agregar archivos de proyecto.

---
+++ Ejer12DatosEmple/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejer12DatosEmple
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Empleados
+            {
+                nombre = txtNombre.Text,
+                apellidos = txtApellidos.Text,
+                edad = txtEdad.Text,
+                email = txtEmail.Text,
+            };
+
+            var datos = new PageData();
+            datos.BindingContext = emple;//.Suma();//Usando Bindign context con un contructor de clas
+            await Navigation.PushAsync(datos);
+
+        }
+    }
+}
---
+++ Ejer12DatosEmple/Models/Empleados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejer12DatosEmple.Models
+{
+   public class Empleados
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string edad { get; set; }
+        public string email { get; set; }
+    }
+}
